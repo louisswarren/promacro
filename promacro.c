cdb63949deea1e2bf7199d0d9298dadc85b7ff25
Louis Warren <louis@lsw.nz>
2023-04-09 14:03:09 +1200

    Tidy up

--- promacro.c
+++ promacro.c
@@ -4,6 +4,7 @@
 
 #include <string.h>
 #include <inttypes.h>
+#include <errno.h>
 
 #include <fcntl.h>
 #include <linux/uinput.h>
@@ -16,34 +17,15 @@
 		exit(1); \
 	} while(0)
 
-static const struct input_event
-	ev_x_down     = {.type = EV_KEY, .code = KEY_X,     .value = 1},
-	ev_x_up       = {.type = EV_KEY, .code = KEY_X,     .value = 0},
-	ev_space_down = {.type = EV_KEY, .code = KEY_SPACE, .value = 1},
-	ev_space_up   = {.type = EV_KEY, .code = KEY_SPACE, .value = 0},
-	ev_syn        = {.type = EV_SYN, .code = SYN_REPORT};
-
 void
 send_event(int fd, const struct input_event *e)
 {
+	static const struct input_event syn = {
+		.type = EV_SYN,
+		.code = SYN_REPORT};
+
 	write(fd, e, sizeof(*e));
-	write(fd, &ev_syn, sizeof(ev_syn));
-}
-
-void
-send_turbo_button(int fd, int pos)
-{
-	static int current_pos = 0;
-	if (pos < 0) {
-		current_pos = !current_pos;
-	} else {
-		current_pos = pos;
-	}
-
-	if (current_pos)
-		send_event(fd, &ev_x_down);
-	else
-		send_event(fd, &ev_x_up);
+	write(fd, &syn, sizeof(syn));
 }
 
 int
@@ -51,33 +33,46 @@ main(void)
 {
 	int turbo_x = 0, turbo_y = 0;
 
-	unsigned char event[8] = {0};
-
-	struct pollfd joyinput;
-	joyinput.events = POLLIN;
-	joyinput.fd = open("/dev/input/js0", O_RDONLY);
-	if (joyinput.fd < 0)
-		die("Failed to open /dev/input/js0");
-
-	struct uinput_setup st = {0};
-	st.id.bustype = BUS_USB;
-	// Just something random
-	st.id.vendor = 0x4C53;
-	st.id.product = 0x5357;
-	strcpy(st.name, "Joy Emulated");
+	/* Set up virtual keyboard */
+	struct uinput_setup st = {
+		.name = "Joy Emulated",
+		.id.bustype = BUS_USB,
+		/* My initials - doesn't matter */
+		.id.vendor = 0x4C53,
+		.id.product = 0x5357};
 
 	int uinput_fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
 	if (uinput_fd < 0) {
 		die("Could not open /dev/uinput");
 	}
 
-	if (-1 == ioctl(uinput_fd, UI_SET_EVBIT, EV_KEY)
-	 || -1 == ioctl(uinput_fd, UI_SET_KEYBIT, KEY_X)
-	 || -1 == ioctl(uinput_fd, UI_SET_KEYBIT, KEY_SPACE)
-	 || -1 == ioctl(uinput_fd, UI_DEV_SETUP, &st)
-	 || -1 == ioctl(uinput_fd, UI_DEV_CREATE)) {
-		die("ioctl failed");
+	static struct input_event
+		b_space = {.type = EV_KEY, .code = KEY_SPACE, .value = 0},
+		b_x     = {.type = EV_KEY, .code = KEY_X,     .value = 0};
+
+	if (0 > ioctl(uinput_fd, UI_SET_EVBIT, EV_KEY)
+	 || 0 > ioctl(uinput_fd, UI_SET_KEYBIT, b_space.code)
+	 || 0 > ioctl(uinput_fd, UI_SET_KEYBIT, b_x.code)
+	 || 0 > ioctl(uinput_fd, UI_DEV_SETUP, &st)
+	 || 0 > ioctl(uinput_fd, UI_DEV_CREATE)) {
+		die("Failed to setup uinput (ioctl failed)");
 	}
+
+	/* Wait for joystick to be plugged in */
+	struct pollfd joyinput = {.events = POLLIN, .fd = -1};
+retry:
+	printf("Waiting for joystick ...\n");
+	while(1) {
+		joyinput.fd = open("/dev/input/js0", O_RDONLY);
+		if (joyinput.fd >= 0)
+			break;
+		if (errno != ENOENT) {
+			perror("Failed to open /dev/input/js0");
+			exit(1);
+		}
+		sleep(1);
+	}
+	printf("Ready.\n");
 
 	while (1) {
 		int ready = 0;
@@ -85,24 +80,28 @@ main(void)
 			/* Poll for event */
 			ready = poll(&joyinput, 1, 16);
 			/* Toggle the turbo button */
-			send_turbo_button(uinput_fd, -1);
+			b_x.value ^= 1;
+			send_event(uinput_fd, &b_x);
 		}
 		/* If turbo is on, this shouldn't block */
+		unsigned char event[8] = {0};
 		if (read(joyinput.fd, event, sizeof(event)) < 0) {
-			die("Read error");
+			if (errno == ENODEV)
+				goto retry;
+			perror("Read error");
+			exit(1);
 		}
 
-		uint16_t meta = (event[5] <<  8) | event[4];
-		int16_t axis;
-		memcpy(&axis, &meta, 2);
+		uint16_t meta = (event[5] << 8) | event[4];
+		int16_t axis = meta >= 0x8000 ? (int32_t)meta - 0x10000l : meta;
 		uint16_t button = (event[6] <<  8) | event[7];
+
+		//printf("%04x\n", button);
 
 		switch (button) {
 		case 0x104:
-			if (axis)
-				send_event(uinput_fd, &ev_space_down);
-			else
-				send_event(uinput_fd, &ev_space_up);
+			b_space.value = !!axis;
+			send_event(uinput_fd, &b_space);
 			break;
 		case 0x203:
 			if (axis > -0x4000 && axis <= 0x4000) {
@@ -110,8 +109,10 @@ main(void)
 			} else {
 				turbo_x = 1;
 			}
-			if (!turbo_x && !turbo_y)
-				send_turbo_button(uinput_fd, 0);
+			if (!turbo_x && !turbo_y) {
+				b_x.value = 0;
+				send_event(uinput_fd, &b_x);
+			}
 			break;
 		case 0x204:
 			if (axis > -0x4000 && axis <= 0x4000) {
@@ -119,8 +120,10 @@ main(void)
 			} else {
 				turbo_y = 1;
 			}
-			if (!turbo_x && !turbo_y)
-				send_turbo_button(uinput_fd, 0);
+			if (!turbo_x && !turbo_y) {
+				b_x.value = 0;
+				send_event(uinput_fd, &b_x);
+			}
 			break;
 		}
 	}
